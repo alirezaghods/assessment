0086608c1e098611ae8a68527f207f50e8025752
Alireza Ghods <36386730+alirezaghods@users.noreply.github.com>
2023-06-27 11:20:29 -0700

    Update utils_lib5.py

--- utils_lib5.py
+++ utils_lib5.py
@@ -99,25 +99,45 @@ class InteractivePlot:
 
         return column(self.p, select)
 
-    def plot_with_fixed_window(self, window_start, window_end):
-        # create the range tool (smaller plot)
-        select = figure(height=100, width=600, y_range=self.p.y_range,
-                        x_axis_type=None, y_axis_type=None,
-                        tools="", toolbar_location=None, background_fill_color="#efefef")
+    # def plot_with_fixed_window(self, window_start, window_end):
+    #     # create the range tool (smaller plot)
+    #     select = figure(height=100, width=600, y_range=self.p.y_range,
+    #                     x_axis_type=None, y_axis_type=None,
+    #                     tools="", toolbar_location=None, background_fill_color="#efefef")
 
-        # set the fixed window size to the main plot's x_range
-        self.p.x_range = Range1d(window_start, window_end)
+    #     # set the fixed window size to the main plot's x_range
+    #     self.p.x_range = Range1d(window_start, window_end)
 
-        range_tool = RangeTool(x_range=self.p.x_range)
-        range_tool.overlay.fill_color = "navy"
-        range_tool.overlay.fill_alpha = 0.2
+    #     range_tool = RangeTool(x_range=self.p.x_range)
+    #     range_tool.overlay.fill_color = "navy"
+    #     range_tool.overlay.fill_alpha = 0.2
 
-        select.line('x', 'y', source=self.source)
-        select.ygrid.grid_line_color = None
-        select.add_tools(range_tool)
-        select.toolbar.active_multi = range_tool
+    #     select.line('x', 'y', source=self.source)
+    #     select.ygrid.grid_line_color = None
+    #     select.add_tools(range_tool)
+    #     select.toolbar.active_multi = range_tool
 
-        return column(self.p, select)
+    #     return column(self.p, select)
+def plot_with_fixed_window(window_start, window_end):
+    # create the range tool (smaller plot)
+    select = figure(height=100, width=600, y_range=p.y_range,
+                    x_axis_type=None, y_axis_type=None,
+                    tools="", toolbar_location=None, background_fill_color="#efefef")
+
+    # set the fixed window size to the main plot's x_range
+    p.x_range.start = window_start
+    p.x_range.end = window_end
+
+    range_tool = RangeTool(x_range=p.x_range)
+    range_tool.overlay.fill_color = "navy"
+    range_tool.overlay.fill_alpha = 0.2
+
+    select.line('x', 'y', source=source)
+    select.ygrid.grid_line_color = None
+    select.add_tools(range_tool)
+    select.toolbar.active_multi = range_tool
+
+    return column(p, select)
 
 
 
